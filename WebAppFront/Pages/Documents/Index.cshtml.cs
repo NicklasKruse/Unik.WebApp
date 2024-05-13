5434f97c1d5b48303a0dd6c9729e630b5a7b8c84
Nicklas Kruse <128633229+NicklasKruse@users.noreply.github.com>
2024-05-13 12:55:10 +0200

    Inden chathub

--- WebAppFront/Pages/Documents/Index.cshtml.cs
+++ WebAppFront/Pages/Documents/Index.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.CompilerServices;
+using WebAppFront.Services.Interfaces;
 
 namespace WebAppFront.Pages.Documents
 {
     public class IndexModel : PageModel
     {
+        private readonly IFileUploadService _fileUploadService;
+
+        public IndexModel(IFileUploadService fileUploadService)
+        {
+            _fileUploadService = fileUploadService;
+        }
+
+        public List<string> Files { get; private set; }
         public void OnGet()
         {
+            Files = _fileUploadService.ListFiles().ToList();
         }
     }
 }
