4eed20d8d191d807c8198eeb8abe2faf2bc6aea9
Nicklas Kruse <128633229+NicklasKruse@users.noreply.github.com>
2024-05-13 10:36:37 +0200

    auth documents

--- WebAppFront/Pages/UploadFile/Upload.cshtml.cs
+++ WebAppFront/Pages/UploadFile/Upload.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebAppFront.Services.Interfaces;
 
+[Authorize(Roles = "Formand, Bestyrelse")]
 public class UploadModel : PageModel
 {
     private readonly IFileUploadService _fileUploadService;
