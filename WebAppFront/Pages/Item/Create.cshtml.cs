d119a7dee7305505fd3e642c300fe97b79bae143
Nicklas Kruse <128633229+NicklasKruse@users.noreply.github.com>
2024-05-13 10:50:57 +0200

    Inden tilføjelse af concurrency handling i dbcontext

--- WebAppFront/Pages/Item/Create.cshtml.cs
+++ WebAppFront/Pages/Item/Create.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebAppFront.Pages.Item
 {
+    [Authorize(Roles = "Formand, Bestyrelse")]
     public class CreateModel : PageModel
     {
         public void OnGet()
--- WebAppFront/Pages/Item/Index.cshtml.cs
+++ WebAppFront/Pages/Item/Index.cshtml.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebAppFront.Services.Interfaces;
 
 namespace WebAppFront.Pages.Item
 {
+    [Authorize(Roles = "Formand, Bestyrelse")]
     public class IndexModel : PageModel
     {
         private readonly IItemService itemService;
